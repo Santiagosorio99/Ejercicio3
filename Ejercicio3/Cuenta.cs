105939e5d572af9b57ff6b962736fac2f89f77c9
Santiagoneo24@outlook.com <Santiagoneo24@outlook.com>
2021-02-24 16:01:19 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio3/Cuenta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Ejercicio3
+{
+    public class Cuenta
+    {
+        public Cuenta()
+        {
+
+        }
+
+        public double Saldo { get; set; }
+        public string Numero { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Retirar(double valor)
+        {
+            if (valor <= Saldo)
+            {
+                Saldo -= valor;
+                return $"se hizo un retiro de {valor} y su saldo es de {Saldo}";
+     
+            }
+            return "saldo insuficiente";
+        }
+        public string Consignar(double valor)
+        {
+            if (valor > 0)
+            {
+                Saldo += valor;
+                return $"Se hizo una consignación de {valor} y su saldo es de {Saldo}";
+            }
+            return "no es posible realizar esta consignación";
+        }
+        public double ConsultarSaldo()
+        {
+            return Saldo;
+        }
+    }
+}
---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta cuenta = new Cuenta() {
+
+                Numero = "1",
+                Saldo = 2000,
+                Fecha = Convert.ToDateTime("01/03/2020")
+            
+            };
+            Console.WriteLine(cuenta.Retirar(500));
+            Console.WriteLine(cuenta.Consignar(600));
+            Console.WriteLine(cuenta.ConsultarSaldo());
+            Console.ReadLine();
+        }
+    }
+}
